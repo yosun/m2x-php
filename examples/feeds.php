898882ce5dff3e5af8b51ffbefe2a09287da6a4a
Leandro López (inkel) <inkel.ar@gmail.com>
2013-10-24 10:41:27 -0300

    Improve example

--- examples/feeds.php
+++ examples/feeds.php
@@ -2,45 +2,50 @@
 
 include_once "lib/m2x.php";
 
-$m2x = new M2X("<API KEY HERE>");
+$api_key = "<API KEY HERE>";
+$feed_id = "<FEED>";
+$stream  = "<STREAM NAME>";
 
-//VIEW FEED
-$response = $m2x->feeds()->view("<FEED>");
+$m2x = new M2X($api_key);
 
-//LIST STREAMS
-$response = $m2x->feeds()->streams("<FEED>");
+// View Feed
+$response = $m2x->feeds()->view($feed_id);
 
-//GET DETAILS FROM EXISTING STREAM
-$response = $m2x->feeds()->stream("<FEED>", "<STREAM NAME>");
-  
-//READ VALUES FROM EXISTING STREAM
-$response = $m2x->feeds()->stream_values("<FEED>", "<STREAM NAME>");
+// Create/Update Stream
+$data = array(
+  "value" => 1.23,
+  "unit"  => array("label" => "Celsius")
+);
+$response = $m2x->feeds()->update_stream($feed_id, $stream, $data);
 
-//POST VALUES FROM EXISTING STREAM
+// List Streams
+$response = $m2x->feeds()->streams($feed_id);
 
-//METHOD 1
-$response = $m2x->feeds()->add_stream_values("<FEED>", "<STREAM NAME>", array(
-  array("value" => "<VALUE>"),
-  array("value" => "<VALUE>"),
-  array("value" => "<VALUE>")
-));
-//METHOD 2
-$response = $m2x->feeds()->update_stream("<FEED>", "<STREAM NAME>", array("value" => "<VALUE>"));
+// Get Details From Existing Stream
+$response = $m2x->feeds()->stream($feed_id, $stream);
 
-//READ LOCATION INFORMATION
-$response = $m2x->feeds()->location("<FEED>");
+// Read Values From Existing Stream
+$response = $m2x->feeds()->stream_values($feed_id, $stream);
 
-//UPDATE LOCATION INFORMATION
-$response = $m2x->feeds()->update_location("<FEED>", array(
-  "name" => "<NAME>",
-  "latitude" => "<LATITUDE>",
-  "longitude" => "<LONGITUDE>",
-  "elevation" => "<ELEVATION>"
+// Post Multiple Values To Stream
+$response = $m2x->feeds()->add_stream_values($feed_id, $stream, array(
+  array("value" => 456),
+  array("value" => 789),
+  array("value" => 123.145)
 ));
 
+// Read Location Information
+$response = $m2x->feeds()->location($feed_id);
 
-// RESPONSE OBJECT  
-$code = $response.code(); //HTTP STATUS CODE
-$header = $response.headers(); //RESPONSE HEADER
-$raw = $response.raw(); //RAW BODY
-$json = $response.json(); //PARSED BODY
+// Update Location Information
+$response = $m2x->feeds()->update_location($feed_id, array(
+  "name"      => "Seattle",
+  "latitude"  => 47.6097,
+  "longitude" => 122.3331
+));
+
+// Response Object
+$code   = $response->code();    // HTTP Status Code
+$header = $response->headers(); // Response Header
+$raw    = $response->raw();     // Raw Body
+$json   = $response->json();    // Parsed Body
