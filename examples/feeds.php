d00026fb4d4406159cdb9e31565d2bf02c337792
Lautaro Orazi <orazile@gmail.com>
2013-11-13 17:05:05 -0300

    Update examples

--- examples/feeds.php
+++ examples/feeds.php
@@ -11,6 +11,14 @@ $m2x = new M2X($api_key);
 // View Feed
 $response = $m2x->feeds()->view($feed_id);
 
+//Search Feed
+$query = "<SEARCH STRING>";
+$response = $m2x->feeds()->search($query, array(
+  "page" => 1,
+  "limit" => 100,
+  "tags" => "tag1,tag2"
+));
+
 // Create/Update Stream
 $data = array(
   "value" => 1.23,
@@ -19,7 +27,10 @@ $data = array(
 $response = $m2x->feeds()->update_stream($feed_id, $stream, $data);
 
 // List Streams
-$response = $m2x->feeds()->streams($feed_id);
+$from_date = "2013-09-09T00:00:00Z";
+$to_date = "2013-09-13T00:00:00Z";
+$limit = 100;
+$response = $m2x->feeds()->streams($feed_id, $from_date, $to_date, $limit);
 
 // Get Details From Existing Stream
 $response = $m2x->feeds()->stream($feed_id, $stream);
@@ -32,6 +43,20 @@ $response = $m2x->feeds()->add_stream_values($feed_id, $stream, array(
   array("value" => 456),
   array("value" => 789),
   array("value" => 123.145)
+));
+
+//Post Multiple Values to Multiple Streams
+$response = $m2x->feeds()->post_multiple($feed_id, array(
+  "<STREAM NAME>" => array(
+    array("at" => "2013-09-09T19:15:00Z", "value" => 456),
+    array("value" => 789),
+    array("value" => 123.145)
+  ),
+  "<STREAM NAME>" => array(
+    array("at" => "2013-09-09T19:15:00Z", "value" => 456),
+    array("value" => 789),
+    array("value" => 123.145)
+  )
 ));
 
 // Read Location Information
